9cb46d2423979a7f9e026c8c7fd4fb18cccff403
Ntvbh <bedzhashe@aicrobotics.ru>
2021-09-30 12:16:27 +0300

    lazy load added

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,60 @@
 
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './Home.scss'
 
 const Home = () => {
+  const [posts, setPosts] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [fetching, setFetching] = useState(true)
+  const [totalCount, setTotalCount] = useState(0)
+
+
+  const getPost = () => {
+    if(fetching) {
+      // console.log('загрузка ' + result.data);
+      axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=20&_page=${currentPage}`)
+      .then((result) => {
+        setPosts([...posts, ...result.data])
+        setCurrentPage(prevState => prevState + 1)
+        setTotalCount(result.headers['x-total-count'])
+      })
+      .finally(() => setFetching(false))
+    }
+  }
+
+  const scrollHandler = (e) => {
+    if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100 && posts.length < totalCount) {
+      setFetching(true)
+    }
+  }
+
+
+  const lazyLoad = () => {
+    document.addEventListener('scroll', scrollHandler)
+
+    return function() {
+      document.removeEventListener('scroll', scrollHandler)
+    }
+  }
+
+  useEffect(() => {
+    getPost()
+    lazyLoad()
+  }, [fetching])
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>l
-        zzzzzzzzzzzzzzzzzzzz
-      </p>
+    <div className='home'>
+      <div className='home__post-block'>
+        {posts.map((item) => (
+          <div key={item.id} className='home__post-block__posts' >
+            <p className='home__post-block__post-title' >{ item.title }</p>
+            <p className='home__post-block__post-body' >{ item.body }</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
